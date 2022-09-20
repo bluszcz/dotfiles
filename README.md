5a1a0436da20378485311e2f0678798ae57545fb
Rafał Zawadzki <bluszcz@bluszcz.net>
2022-09-20 11:52:50 +0200

    Added nodegroup to EKS cluster

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Various dotfiles - to simplify copying them between environments.
 
 ```curl https://raw.githubusercontent.com/bluszcz/dotfiles/main/vim/.vimrc -o ~/.vimrc```
 
+## install DevOps Stack
+
+[Install DevOps Stack](INSTALL-devops.md)
--- tf-dc-cloud/aws/main.tf
+++ tf-dc-cloud/aws/main.tf
@@ -115,7 +115,7 @@ associate_public_ip_address = true
       type        = "ssh"
       host        = self.public_ip
       user        = "ubuntu"
-      private_key = file("/home/bluszcz/.ssh/id_rsa")
+      private_key = file("/home/bluszcz/.ssh/cloud_key")
       timeout     = "4m"
    }
 
@@ -164,4 +164,60 @@ resource "aws_eks_cluster" "dev-cluster" {
 
 
 }
-  
+
+resource "aws_iam_role" "role_nodegroup" {
+  name = "eks-node-group-example"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.role_nodegroup.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.role_nodegroup.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.role_nodegroup.name
+}
+
+resource "aws_eks_node_group" "example" {
+  cluster_name    = aws_eks_cluster.dev-cluster.name
+  node_group_name = "example"
+  node_role_arn   = aws_iam_role.role_nodegroup.arn
+  subnet_ids = [aws_subnet.my_cluster_subnet1.id, aws_subnet.my_cluster_subnet2.id]
+  instance_types = ["t2.medium", "t3.medium"]
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
+
